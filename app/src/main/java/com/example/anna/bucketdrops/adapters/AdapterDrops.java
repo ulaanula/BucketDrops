c305b3ceb052ce21d2ade49496475bdd3f5221d1
Anna <annaulkowska@yahoo.com>
2016-08-30 17:49:58 +0200

    Add footer for RecyclerView

--- app/src/main/java/com/example/anna/bucketdrops/adapters/AdapterDrops.java
+++ app/src/main/java/com/example/anna/bucketdrops/adapters/AdapterDrops.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.anna.bucketdrops.R;
@@ -16,11 +17,13 @@ import io.realm.RealmResults;
 /**
  * Created by Anna on 26.08.2016.
  */
-public class AdapterDrops extends RecyclerView.Adapter<AdapterDrops.DropHolder> {
+public class AdapterDrops extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     private LayoutInflater mInflator;
     private RealmResults<Drop> mResults;
     public static final String TAG ="ANNA";
+    public static final int ITEM=0;
+    public static final int FOOTER=1;
 
     public AdapterDrops(Context context, RealmResults<Drop> results){
 
@@ -34,29 +37,46 @@ public class AdapterDrops extends RecyclerView.Adapter<AdapterDrops.DropHolder> 
     }
 
     @Override
-    public DropHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public int getItemViewType(int position) {
 
-       View view = mInflator.inflate(R.layout.row_drop,parent,false);
-
-       DropHolder holder = new DropHolder(view);
-
-        Log.d(TAG, "onCreateViewHolder: ");
-
-        return holder;
+        if(mResults ==null || position<mResults.size()){
+            return ITEM;
+        }else{
+            return FOOTER;
+        }
     }
 
     @Override
-    public void onBindViewHolder(DropHolder holder, int position) {
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Log.d(TAG, "onCreateViewHolder: ");
+        if(viewType==FOOTER){
+            View view = mInflator.inflate(R.layout.footer,parent,false);
+            FooterHolder holder = new FooterHolder(view);
+            return holder;
+        }else{
+            View view = mInflator.inflate(R.layout.row_drop,parent,false);
+            DropHolder holder = new DropHolder(view);
+            return holder;
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
         Log.d(TAG, "onBindViewHolder: " + position);
 
-        Drop drop = mResults.get(position);
-        holder.textWhat.setText(drop.getWhat());
+        if(holder instanceof DropHolder){
+            Drop drop = mResults.get(position);
+            DropHolder dropHolder = (DropHolder) holder;
+            dropHolder.textWhat.setText(drop.getWhat());
+        }
+
 
     }
 
     @Override
     public int getItemCount() {
-        return mResults.size();
+        // +1 is because of footer
+        return mResults.size()+1;
     }
 
     public static class DropHolder extends RecyclerView.ViewHolder{
@@ -66,6 +86,16 @@ public class AdapterDrops extends RecyclerView.Adapter<AdapterDrops.DropHolder> 
         public DropHolder(View itemView) {
             super(itemView);
             textWhat = (TextView)itemView.findViewById(R.id.tv_what);
+        }
+    }
+
+    public static class FooterHolder extends RecyclerView.ViewHolder{
+
+        Button btnAdd;
+
+        public FooterHolder(View itemView) {
+            super(itemView);
+            btnAdd = (Button) itemView.findViewById(R.id.btn_footer);
         }
     }
 }
